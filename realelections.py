1e899ac082a24678eeb240f14b6f04ceeb2bc60c
Ahmed Suleiman <94101874+himilo123@users.noreply.github.com>
2022-12-11 20:49:49 +0300

    Update realelections.py

--- realelections.py
+++ realelections.py
@@ -64,31 +64,37 @@ def himilo(var1):
     fig1 = px.choropleth(dff, geojson=geojson1, locations='name', color='Kulmiye', featureidkey="properties.name",
                          title='Kulmiye Parliment Seats')
     fig1.update_geos(fitbounds="locations", visible=False)
+    fig1.layout.update(dragmode=False)
 
     dff = df1[(df1["Year"] == var1)]
     fig2 = px.choropleth(dff, geojson=geojson1, locations='name', color='Wadani', featureidkey="properties.name",
                          title='Wadani Parliment Seats')
     fig2.update_geos(fitbounds="locations", visible=False)
+    fig2.layout.update(dragmode=False)
 
     dff = df1[(df1["Year"] == var1)]
     fig3 = px.choropleth(dff, geojson=geojson1, locations='name', color='Ucid', featureidkey="properties.name",
                          title='Ucid Parliment Seats')
     fig3.update_geos(fitbounds="locations", visible=False)
+    fig3.layout.update(dragmode=False)
 
     dff = df1[(df1["Year"] == var1)]
     fig4 = px.choropleth(dff, geojson=geojson2, locations='name', color='kulmiye', featureidkey="properties.name",
                          title='Kulmiye Municiplity Seats')
     fig4.update_geos(fitbounds="locations", visible=False)
+    fig4.layout.update(dragmode=False)
 
     dff = df1[(df1["Year"] == var1)]
     fig5 = px.choropleth(dff, geojson=geojson2, locations='name', color='wadani', featureidkey="properties.name",
                          title='Wadani Municiplity Seats')
     fig5.update_geos(fitbounds="locations", visible=False)
+    fig5.layout.update(dragmode=False)
 
     dff = df1[(df1["Year"] == var1)]
     fig6 = px.choropleth(dff, geojson=geojson2, locations='name', color='ucid', featureidkey="properties.name",
                          title='Ucid Municiplity Seats')
     fig6.update_geos(fitbounds="locations", visible=False)
+    fig6.layout.update(dragmode=False)
 
     return fig1, fig2, fig3, fig4, fig5, fig6
 
