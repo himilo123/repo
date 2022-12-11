ccb9ff8bb29ebb7ead5a02bd958bd1e0b4c67d1a
Ahmed Suleiman <94101874+himilo123@users.noreply.github.com>
2022-12-11 15:06:54 +0300

    Update elections.py

--- elections.py
+++ elections.py
@@ -10,10 +10,13 @@ import pandas as pd
  #owner: Dustin Childers on Kaggle, source: https://data.brla.gov/Public-Safety/Animal-Control-Incidents/qmns-hw3s
 df = pd.read_csv("elections.csv")
 
-app = dash.Dash(__name__,
-                meta_tags=[{'name': 'viewport',
-                             'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}]
-                )
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    meta_tags=[
+        {"name": "viewport",
+         "content": "width=device-width, initial-scale=1.0, maximum-scale=2, minimum-scale=0.5"}
+    ],
+)
 
 
 title = html.H1("Somaliland Presidential Eelections", style={'textAlign': 'center'})
