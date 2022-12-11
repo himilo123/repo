ef615adc5cf9e3f54d011c61857aa5a1500e65ff
Ahmed Suleiman <94101874+himilo123@users.noreply.github.com>
2022-12-11 10:18:36 +0300

    Update regions.py

--- regions.py
+++ regions.py
@@ -19,7 +19,7 @@ app = dash.Dash(
     external_stylesheets=[dbc.themes.CYBORG],
     meta_tags=[
         {"name": "viewport",
-         "content": "width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1"}
+         "content": "width=device-width, initial-scale=1, maximum-scale=2, minimum-scale=0.5"}
     ],
 )
 header = html.H5("Himilo Data Solutions & Research", style={'textAlign': 'center'})
