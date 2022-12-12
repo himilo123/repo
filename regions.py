70888eafb5fb600a184563e1b38097c37be891bb
Ahmed Suleiman <94101874+himilo123@users.noreply.github.com>
2022-12-12 07:27:15 +0300

    Update regions.py

--- regions.py
+++ regions.py
@@ -16,14 +16,9 @@ from dash import html
 from dash.dependencies import Output, Input
 
 app = dash.Dash(
-    external_stylesheets=[dbc.themes.BOOTSTRAP],
-    meta_tags=[
-        {"name": "viewport",
-         "content": "width=device-width, initial-scale=1.0, maximum-scale=2, minimum-scale=0.5"}
-    ],
-)
-header = html.H5("Himilo Data Solutions & Research", style={'textAlign': 'center'})
-title = html.H6("Somaliland Eelections Sample Data", style={'textAlign': 'center'})
+    external_stylesheets=[dbc.themes.BOOTSTRAP])
+header = html.H2("Himilo Data Solutions & Research Center", style={'textAlign': 'center'})
+title = html.H5("Somaliland Parliamentary and Municipality Voting  Analysis", style={'textAlign': 'center'})
 
 with open(r'json-sml-regions.txt') as j1:
     geojson1 = json.load(j1)
@@ -33,11 +28,10 @@ with open(r'json-sml-districts.txt') as j2:
     geojson2 = json.load(j2)
 geojson2 = rewind(geojson2, rfc7946=False)
 
-df1 = pd.read_csv(r'regions-district.csv')
+df1 = pd.read_csv(r'realelectons.csv')
 
 input1 = dbc.Col(dcc.RadioItems(id='drop1', options=[{'label': x, 'value': x} for x in df1['Year'].unique()],
                                 value=2021))
-
 
 output1 = dbc.Col(dcc.Graph(id='myfig1', figure={}),xs=12, sm=12, md=12, lg=4, xl=4)
 output2 = dbc.Col(dcc.Graph(id='myfig2', figure={}),xs=12, sm=12, md=12, lg=4, xl=4)
@@ -45,9 +39,10 @@ output3 = dbc.Col(dcc.Graph(id='myfig3', figure={}),xs=12, sm=12, md=10, lg=4, x
 output4 = dbc.Col(dcc.Graph(id='myfig4', figure={}),xs=12, sm=12, md=12, lg=4, xl=4)
 output5 = dbc.Col(dcc.Graph(id='myfig5', figure={}),xs=12, sm=12, md=12, lg=4, xl=4)
 output6 = dbc.Col(dcc.Graph(id='myfig6', figure={}),xs=12, sm=12, md=12, lg=4, xl=4)
+
 # row1 = [input1, style={'textAlign': 'center'}]
 
-row1 = html.H6(["Select The Year", input1], style={'textAlign': 'center'})
+row1 = html.H6([ input1], style={'textAlign': 'center'})
 row2 = dbc.Row([output1, output2, output3])
 row3 = dbc.Row([output4, output5, output6])
 
@@ -65,44 +60,49 @@ app.layout = dbc.Container([header,title, row1, row2, row3])
 )
 def himilo(var1):
     dff = df1[(df1["Year"] == var1)]
-    fig1 = px.choropleth(dff, geojson=geojson1, locations='name', color='Kulmiye_Regional_Seats', featureidkey="properties.name",
-                         title='Kulmiye Votes% Regional Level')
+    fig1 = px.choropleth(dff, geojson=geojson1, locations='name', color='Kulmiye%', featureidkey="properties.name",range_color=(20, 40),
+                         title='Kulmiye Parliment Seats', hover_data=["Total","Kulmiye", "Kulmiye%"])
     fig1.update_geos(fitbounds="locations", visible=False)
+    fig1.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig1.layout.update(dragmode=False)
 
-
     dff = df1[(df1["Year"] == var1)]
-    fig2 = px.choropleth(dff, geojson=geojson1, locations='name', color='Wadani_Regional_Seats', featureidkey="properties.name",
-                         title='Wadani Votes% Regional Level')
+    fig2 = px.choropleth(dff, geojson=geojson1, locations='name', color='Wadani%', featureidkey="properties.name",range_color=(20, 40),
+                         title='Wadani Parliment Seats', hover_data=["Total","Wadani", "Wadani%"])
     fig2.update_geos(fitbounds="locations", visible=False)
+    fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig2.layout.update(dragmode=False)
 
     dff = df1[(df1["Year"] == var1)]
-    fig3 = px.choropleth(dff, geojson=geojson1, locations='name', color='Ucid_Regional_Seats', featureidkey="properties.name",
-                         title='Ucid Votes% Regional Level')
+    fig3 = px.choropleth(dff, geojson=geojson1, locations='name', color='Ucid%', featureidkey="properties.name",range_color=(20, 40),
+                         title='Ucid Parliment Seats', hover_data=["Total","Ucid", "Ucid%"])
     fig3.update_geos(fitbounds="locations", visible=False)
+    fig3.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig3.layout.update(dragmode=False)
 
     dff = df1[(df1["Year"] == var1)]
-    fig4 = px.choropleth(dff, geojson=geojson2, locations='name', color='Kulmiye_District_Seats', featureidkey="properties.name",
-                         title='Kulmiye Votes% District Level')
+    fig4 = px.choropleth(dff, geojson=geojson2, locations='name', color='kulmiye%', featureidkey="properties.name",range_color=(0, 50),
+                         title='Kulmiye Municiplity Seats', color_continuous_scale="Viridis",hover_data=["total","kulmiye", "kulmiye%"])
     fig4.update_geos(fitbounds="locations", visible=False)
+    fig4.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig4.layout.update(dragmode=False)
 
     dff = df1[(df1["Year"] == var1)]
-    fig5 = px.choropleth(dff, geojson=geojson2, locations='name', color='Wadani_District_Seats', featureidkey="properties.name",
-                         title='Wadani Votes% District Level')
+    fig5 = px.choropleth(dff, geojson=geojson2, locations='name', color='wadani%', featureidkey="properties.name",range_color=(0, 50),
+                         title='Wadani Municiplity Seats',color_continuous_scale="Viridis", hover_data=["total","wadani", "wadani%"])
     fig5.update_geos(fitbounds="locations", visible=False)
+    fig5.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig5.layout.update(dragmode=False)
 
     dff = df1[(df1["Year"] == var1)]
-    fig6 = px.choropleth(dff, geojson=geojson2, locations='name', color='Ucid_District_Seats', featureidkey="properties.name",
-                         title='Ucid Votes% District Level')
+    fig6 = px.choropleth(dff, geojson=geojson2, locations='name', color='ucid%', featureidkey="properties.name",range_color=(0, 50),
+                         title='Ucid Municiplity Seats', color_continuous_scale="Viridis",hover_data=["total","ucid", "ucid%"])
     fig6.update_geos(fitbounds="locations", visible=False)
+    fig6.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig6.layout.update(dragmode=False)
 
     return fig1, fig2, fig3, fig4, fig5, fig6
 
 app.run(host='0.0.0.0')
-
+#app.run(port = 4089)
 server = app.server
