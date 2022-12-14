0b6af6708b5ea47b361a835623583430eb51bee0
Ahmed Suleiman <94101874+himilo123@users.noreply.github.com>
2022-12-15 00:12:49 +0300

    Update elections.py

--- elections.py
+++ elections.py
@@ -17,8 +17,11 @@ from dash.dependencies import Output, Input
 
 app = dash.Dash(
     external_stylesheets=[dbc.themes.BOOTSTRAP])
-header = html.H2("Himilo Data Solutions & Research", style={'textAlign': 'center'})
-title = html.H5("Somaliland Polotical Parties voting pool analysis", style={'textAlign': 'center'})
+header = html.H2("Himilo Data Solutions & Research Center", style={'textAlign': 'center'})
+title = html.H5("Somaliland Parliamentary and Municipality Voting  Analysis", style={'textAlign': 'center'})
+head1= html.P("Labadan sawir ee hoose waxay miijinayaan sidaa looga kala guulayastay doorashooyinkii isku sidkanaa ee  golayaasha degaanka iyo wakiilada ee Somaliland 2021 eyadoo gobol  iyo degmo kasta  la siiyay cida ka heshay kuraasta ugu badan.")
+head2= html.P("Saddexdan  sawir ee hoose waxay miijinayaan sidaa looga kala guulayastay doorashooyinkii ee  golaha wakiilada ee Somaliland 2021 eyadoo la tiranayo tirade kuraasta oo loo bedelay boqolay.")
+head3= html.P("Saddexdan  sawir ee hoose eyagu  waxay miijinayaan sidaa looga kala guulayastay doorashooyinkii ee  golayaasha degaanka  ee Somaliland 2021 eyadoo la tiranayo tirade kuraasta oo loo bedelay boqolay.")
 
 with open(r'json-sml-regions.txt') as j1:
     geojson1 = json.load(j1)
@@ -39,14 +42,16 @@ output3 = dbc.Col(dcc.Graph(id='myfig3', figure={}),xs=12, sm=12, md=10, lg=4, x
 output4 = dbc.Col(dcc.Graph(id='myfig4', figure={}),xs=12, sm=12, md=12, lg=4, xl=4)
 output5 = dbc.Col(dcc.Graph(id='myfig5', figure={}),xs=12, sm=12, md=12, lg=4, xl=4)
 output6 = dbc.Col(dcc.Graph(id='myfig6', figure={}),xs=12, sm=12, md=12, lg=4, xl=4)
+output7 = dbc.Col(dcc.Graph(id='myfig7', figure={}),xs=12, sm=12, md=12, lg=6, xl=6)
+output8 = dbc.Col(dcc.Graph(id='myfig8', figure={}),xs=12, sm=12, md=12, lg=6, xl=6)
 
 # row1 = [input1, style={'textAlign': 'center'}]
 
-row1 = html.H6(["Select The Year", input1], style={'textAlign': 'center'})
+row1 = html.H6([ input1], style={'textAlign': 'center'})
 row2 = dbc.Row([output1, output2, output3])
 row3 = dbc.Row([output4, output5, output6])
-
-app.layout = dbc.Container([title, row1, row2, row3])
+row4 = dbc.Row([output7, output8])
+app.layout = dbc.Container([header,title,head1,row4, head2,row1,row2, head3,row3])
 
 
 @app.callback(
@@ -56,42 +61,73 @@ app.layout = dbc.Container([title, row1, row2, row3])
     Output(component_id="myfig4", component_property='figure'),
     Output(component_id="myfig5", component_property='figure'),
     Output(component_id="myfig6", component_property='figure'),
+    Output(component_id="myfig7", component_property='figure'),
+    Output(component_id="myfig8", component_property='figure'),
     Input(component_id='drop1', component_property='value'),
-
 )
 def himilo(var1):
     dff = df1[(df1["Year"] == var1)]
-    fig1 = px.choropleth(dff, geojson=geojson1, locations='name', color='Kulmiye', featureidkey="properties.name",range_color=(0, 8),
-                         title='Kulmiye Parliment Seats')
+    fig1 = px.choropleth(dff, geojson=geojson1, locations='name', color='Kulmiye%', featureidkey="properties.name",range_color=(20, 40),
+                         title='Kulmiye Parliment Seats',color_continuous_scale="Viridis", hover_data=["Total","Kulmiye", "Kulmiye%"])
     fig1.update_geos(fitbounds="locations", visible=False)
+    fig1.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig1.layout.update(dragmode=False)
 
     dff = df1[(df1["Year"] == var1)]
-    fig2 = px.choropleth(dff, geojson=geojson1, locations='name', color='Wadani', featureidkey="properties.name",range_color=(0, 8),
-                         title='Wadani Parliment Seats')
+    fig2 = px.choropleth(dff, geojson=geojson1, locations='name', color='Wadani%', featureidkey="properties.name",range_color=(20, 40),
+                         title='Wadani Parliment Seats',color_continuous_scale="Viridis", hover_data=["Total","Wadani", "Wadani%"])
     fig2.update_geos(fitbounds="locations", visible=False)
+    fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig2.layout.update(dragmode=False)
 
     dff = df1[(df1["Year"] == var1)]
-    fig3 = px.choropleth(dff, geojson=geojson1, locations='name', color='Ucid', featureidkey="properties.name",range_color=(0, 8),
-                         title='Ucid Parliment Seats')
+    fig3 = px.choropleth(dff, geojson=geojson1, locations='name', color='Ucid%', featureidkey="properties.name",range_color=(20, 40),
+                         title='Ucid Parliment Seats', color_continuous_scale="Viridis",hover_data=["Total","Ucid", "Ucid%"])
     fig3.update_geos(fitbounds="locations", visible=False)
+    fig3.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig3.layout.update(dragmode=False)
 
     dff = df1[(df1["Year"] == var1)]
-    fig4 = px.choropleth(dff, geojson=geojson2, locations='name', color='kulmiye', featureidkey="properties.name",range_color=(0, 7),
-                         title='Kulmiye Municiplity Seats')
+    fig4 = px.choropleth(dff, geojson=geojson2, locations='name', color='kulmiye%', featureidkey="properties.name",range_color=(0, 50),
+                         title='Kulmiye Municiplity Seats', color_continuous_scale="Viridis",hover_data=["total","kulmiye", "kulmiye%"])
     fig4.update_geos(fitbounds="locations", visible=False)
+    fig4.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig4.layout.update(dragmode=False)
 
     dff = df1[(df1["Year"] == var1)]
-    fig5 = px.choropleth(dff, geojson=geojson2, locations='name', color='wadani', featureidkey="properties.name",range_color=(0, 7),
-                         title='Wadani Municiplity Seats')
+    fig5 = px.choropleth(dff, geojson=geojson2, locations='name', color='wadani%', featureidkey="properties.name",range_color=(0, 50),
+                         title='Wadani Municiplity Seats',color_continuous_scale="Viridis", hover_data=["total","wadani", "wadani%"])
     fig5.update_geos(fitbounds="locations", visible=False)
+    fig5.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig5.layout.update(dragmode=False)
 
     dff = df1[(df1["Year"] == var1)]
-    fig6 = px.choropleth(dff, geojson=geojson2, locations='name', color='ucid', featureidkey="properties.name",range_color=(0, 7),
-                         title='Ucid Municiplity Seats')
+    fig6 = px.choropleth(dff, geojson=geojson2, locations='name', color='ucid%', featureidkey="properties.name",range_color=(0, 50),
+                         title='Ucid Municiplity Seats', color_continuous_scale="Viridis",hover_data=["total","ucid", "ucid%"])
     fig6.update_geos(fitbounds="locations", visible=False)
+    fig6.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig6.layout.update(dragmode=False)
+    
+    fig7 = px.choropleth(dff, geojson=geojson1, locations='name', color='Won', featureidkey="properties.name",color_discrete_map=
+        {'Kulmiye':'yellow','Wadani':'OrangeRed','Neutral':'blue','No-Elections':'red'},
+                         title='Kulmiye Parliment Seats', color_continuous_scale="Viridis",hover_data=["Total","Kulmiye", "Wadani","Ucid"])
+    fig7.update_geos(fitbounds="locations", visible=False)
+    fig7.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig7.layout.update(dragmode=False)
 
-    return fig1, fig2, fig3, fig4, fig5, fig6
+    
+    dff = df1[(df1["Year"] == var1)]
+    fig8 = px.choropleth(dff, geojson=geojson2, locations='name', color='won',
+                         featureidkey="properties.name",color_discrete_map=
+        {'kulmiye':'yellow','wadani':'OrangeRed','Neutral':'blue','No-Elections':'red'},
+                         title='Kulmiye Municiplity Seats', color_continuous_scale="Viridis",hover_data=["total","kulmiye","wadani","ucid"])
+    fig8.update_geos(fitbounds="locations", visible=False)
+    fig8.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig8.layout.update(dragmode=False)
+    
+
+    return fig1, fig2, fig3, fig4, fig5, fig6,fig7,fig8
 
 app.run(host='0.0.0.0')
-#app.run(port = 4060)
+#app.run(port = 4061)
 server = app.server
